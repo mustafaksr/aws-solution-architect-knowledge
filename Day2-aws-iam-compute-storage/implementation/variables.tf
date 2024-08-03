6172ed0c826dd54ff0d418863b190608ae0b4c5e
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2024-08-03 11:37:23 +0300

    day2 - implementation

--- Day2-aws-iam-compute-storage/implementation/variables.tf
+++ Day2-aws-iam-compute-storage/implementation/variables.tf
@@ -6,7 +6,7 @@ variable "db_instance_master_password" {
 }
 
 variable "output_bucket_name" {
-  description = "Unique name for the S3 bucket to store photos"
+  description = "Unique name for the S3 bucket"
   type        = string
   default = "output-bucket-streamlit-fs57ef"
 }
