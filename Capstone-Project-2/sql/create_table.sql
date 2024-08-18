28ff0ddde87b41417efdb16ddd8bf63e5e945cc7
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2024-08-18 14:24:43 +0300

    delete folder

--- Capstone-Project-2/sql/create_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE sensor_data (
-    sensor_id VARCHAR(50) NOT NULL,
-    temperature DECIMAL(5, 2) NOT NULL,
-    pressure DECIMAL(6, 2) NOT NULL,
-    humidity DECIMAL(5, 2) NOT NULL,
-    event_time BIGINT NOT NULL,
-    PRIMARY KEY (sensor_id, event_time)
-);
